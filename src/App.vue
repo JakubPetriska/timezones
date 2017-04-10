798dd41090419fd8bedd7b9adc183ca26d41c206
Jakub Petriska <jakubp@salsitasoft.com>
2017-04-10 15:02:43 +0200

    added card showing timzone and add timezone button

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,56 @@
     <md-toolbar>
       <h1 class="md-title">Timezones</h1>
     </md-toolbar>
-    
+  
     <div class="main-content">
-      <hello></hello>
+      <md-layout>
+        <md-layout v-for="timezone in timezones"
+        class="timezone-card-wrapper">
+          <timezone-card></timezone-card>
+        </md-layout>
+      </md-layout>
+    </div>
+  
+    <div class="add-button-wrapper">
+      <md-button class="md-icon-button md-raised md-accent"
+                 @click.native="addTimezone">
+        <md-icon>add</md-icon>
+      </md-button>
     </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import TimezoneCard from './components/TimezoneCard'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    TimezoneCard
+  },
+  data: function () {
+    return {
+      timezones: ['San Francisco', 'New York', 'Nein', 'Ja', '', '', '']
+    }
+  },
+  methods: {
+    addTimezone: function (event) {
+      this.timezones.push('')
+    }
   }
 }
 </script>
 
 <style>
 .main-content {
-  padding: 16px;
+  padding: 8px;
+}
+.timezone-card-wrapper {
+  margin: 8px;
+}
+.add-button-wrapper {
+  position: fixed;
+  right: 16px;
+  bottom: 24px;
 }
 </style>
---
+++ src/components/TimezoneCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">3:35 AM</div>
+    </md-card-header>
+  
+    <md-card-content>
+      (UTC-08:00) Pacific Time (US & Canada)
+      <br /> America/Los_Angeles
+    </md-card-content>
+  
+    <md-button>Remove</md-button>
+  </md-card>
+</template>
