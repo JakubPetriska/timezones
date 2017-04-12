fb59f9c945355de6cdcb58d699279864c4bae6e1
Jakub Petriska <jakubp@salsitasoft.com>
2017-04-12 14:11:14 +0200

    new color theme

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import 'vue-material/dist/vue-material.css'
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
 
+Vue.material.registerTheme('default', {
+  primary: 'light-green',
+  accent: {
+    color: 'pink',
+    hue: 400
+  },
+  warn: 'red'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
