b2b441fdae1a865308a15a114fd8cda579562af9
Jakub Petriska <jakubp@salsitasoft.com>
2017-04-24 14:01:11 +0200

    added some tests for rendering and basic functions of all components

--- test/unit/specs/AddTimezoneDialog.spec.js
+++ test/unit/specs/AddTimezoneDialog.spec.js
@@ -1,5 +1,61 @@
 import AddTimezoneDialog from '@/components/AddTimezoneDialog'
 import TestUtils from '../TestUtils.js'
+import Vue from 'vue'
 
-// TODO test what gets rendered
-// TODO test proper searching and scoring (order of results)
+const MOCK_TIMEZONES_COUNT = 5
+
+function getAddTimezoneDialog(propsData) {
+  return TestUtils.getMountedComponent(AddTimezoneDialog, propsData)
+}
+
+function getMockTimezones() {
+  const timezones = []
+  for (let i = 0; i < MOCK_TIMEZONES_COUNT; ++i) {
+    timezones.push({
+      text: 'timezone_' + i + '-text',
+      value: 'timezone_' + i + '-value',
+      utc: [
+        'timezone_' + i + '-utc_1',
+        'timezone_' + i + '-utc_2',
+        'timezone_' + i + '-utc_3'
+      ]
+    })
+  }
+  return timezones
+}
+
+describe('AddTimezoneDialog.vue', () => {
+  it('properly renders the list', done => {
+    const timezones = getMockTimezones()
+    const vm = getAddTimezoneDialog({ timezones })
+    Vue.nextTick(() => {
+      const dialogItemsListElement =
+        vm.$el.querySelector('.add-timezone-dialog-content').children[0]
+      expect(dialogItemsListElement.children.length)
+        .to.equal(timezones.length)
+      for (let i = 0; i < MOCK_TIMEZONES_COUNT; ++i) {
+        const child = dialogItemsListElement.children[i]
+        const timezone = timezones[i]
+        expect(child.children[0].children[0].textContent).to.equal(timezone.value)
+        expect(child.children[0].children[1].textContent).to.equal(timezone.text)
+      }
+      done()
+    })
+  })
+
+  it('displays only relevant results when searching', done => {
+    const timezones = getMockTimezones()
+    const vm = getAddTimezoneDialog({ timezones })
+    vm.searchQuery = 'timezone_1'
+    Vue.nextTick(() => {
+      const dialogItemsListElement =
+        vm.$el.querySelector('.add-timezone-dialog-content').children[0]
+      expect(dialogItemsListElement.children.length).to.equal(1)
+      const itemElement = dialogItemsListElement.children[0]
+      const timezone = timezones[1]
+      expect(itemElement.children[0].children[0].textContent).to.equal(timezone.value)
+      expect(itemElement.children[0].children[1].textContent).to.equal(timezone.text)
+      done()
+    })
+  })
+})
--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,5 +1,6 @@
 import App from '@/App'
 import TestUtils from '../TestUtils.js'
+import Vue from 'vue'
 
 function getApp() {
   return TestUtils.getMountedComponent(App)
@@ -9,34 +10,48 @@ describe('App.vue', () => {
   it('initially has 0 shown timezones', () => {
     expect(getApp().shownTimezones.length).to.equal(0)
   })
-  it('properly adds timezone to shownTimezones', () => {
+
+  it('properly adds timezone to shownTimezones and removes it', () => {
     const vm = getApp()
-    vm.addTimezone(vm.notAddedTimezones[0])
+    expect(vm.shownTimezones.length).to.equal(0)
+    const timezoneToAdd = vm.notAddedTimezones[0]
+    vm.addTimezone(timezoneToAdd)
     expect(vm.shownTimezones.length).to.equal(1)
+    vm.removeTimezone(timezoneToAdd.value)
+    expect(vm.shownTimezones.length).to.equal(0)
   })
-  it('properly renders added timezone', () => {
+
+  it('properly adds timezone to shownTimezones and removes it with clear all', () => {
     const vm = getApp()
-    const timezoneToAddText = vm.notAddedTimezones[0].text
-
-    // TODO fix this test
-    console.log(vm.shownTimezones.length)
-    console.log(timezoneToAddText)
-    console.log(vm.notAddedTimezones[0])
-
-    vm.addTimezone(vm.notAddedTimezones[0])
-    expect(vm.$el.querySelector('#timezone-name').textContent)
-        .to.equal(timezoneToAddText)
+    expect(vm.shownTimezones.length).to.equal(0)
+    const timezoneToAdd = vm.notAddedTimezones[0]
+    vm.addTimezone(timezoneToAdd)
+    expect(vm.shownTimezones.length).to.equal(1)
+    vm.clearAll()
+    expect(vm.shownTimezones.length).to.equal(0)
   })
-  it('one less timezone is available after one is added', () => {
+
+  it('properly renders added timezone', done => {
+    const vm = getApp()
+    // Make sure the component has 0 added timezones otherwise the test is gonna fail
+    expect(vm.shownTimezones.length).to.equal(0)
+    const timezoneToAdd = vm.notAddedTimezones[0]
+    vm.addTimezone(timezoneToAdd)
+    Vue.nextTick(() => {
+      expect(vm.$el.querySelector('#timezone-name').textContent)
+        .to.equal(timezoneToAdd.text)
+      vm.clearAll()
+      done()
+    })
+  })
+
+  it('has one less available timezone after one is added', () => {
     const vm = getApp()
     const availableTimezoneCount = vm.notAddedTimezones.length
     vm.addTimezone(vm.notAddedTimezones[0])
     expect(vm.notAddedTimezones.length).to.equal(availableTimezoneCount - 1)
   })
 })
-
-// TODO test removing of timzones
-// TODO test clearing of timezones
 
 // TODO test initial creation of available timezones map (all timezones with utc are available, all without are not)
 // TODO test that timezone no longer present in available timezones is dropped
